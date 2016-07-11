0c2e7e92955d51cc766e65e16f980c7cccc0125d
0x1mason <etmillin@gmail.com>
2016-07-11 14:42:19 -0400

    Init

---
+++ GribExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grib.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GribExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			using (GribFile file = new GribFile(@".\jpeg.grib2")) {
+				foreach (GribMessage msg in file) {
+					Console.Write(msg.GridType);
+					// do something
+				}
+			}
+
+		    new ManualResetEvent(false).WaitOne();
+		}
+	}
+}
